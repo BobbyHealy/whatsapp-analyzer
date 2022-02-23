8d321c4ae2a77a623f46da6494c87efe696bebbc
robertohealy <healybobby@gmail.com>
2022-02-23 04:50:51 -0500

    somehow managed to save file lol

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
+var multer = require('multer');
+
+const fs = require('fs');
+const { promisify } = require('util');
+const app = require('../app');
+const pipeline = promisify(require("stream").pipeline)
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null,'public')
+  },
+  filename: (req,file,cb) => {
+    cb(null, Date.now() + '-' + file.originalname)
+  }
+});
+
+const upload = multer({storage}).single('file');
+
+router.post('/upload', (req, res) => {
+  upload(req, res, (err) => {
+    if(err) {
+      return res.status(500).json(err);
+    }
+
+    return res.status(200).send(req.file);
+  })
 });
 
 module.exports = router;
--- frontend/src/StartButton.js
+++ frontend/src/StartButton.js
@@ -3,20 +3,27 @@ import { useState, useRef } from 'react';
 
 function StartButton() {
   const [file, setFile] = useState();
+  const [name, setName] = useState();
   const fileInputRef = useRef();
 
   const handleChange = (event) => {
     const file1 = event.target.files[0]
     setFile(file1);
+    setName("Name");
   }
 
   const send = event => {
     const data = new FormData();
+    data.append("name", name);
     data.append("file", file);
 
-    axios.post("https://httpbin.org/anything", data)
+    axios.post("http://localhost:3000/upload", data)
       .then(res => console.log(res))
       .catch(err => console.log(err));
+
+    // axios.post("https://httpbin.org/anything", data)
+    //   .then(res => console.log(res))
+    //   .catch(err => console.log(err));
   }
   
   return (
@@ -27,7 +34,7 @@ function StartButton() {
       <button onClick={send}>
         submit!
       </button>
-      <input type="file" ref={fileInputRef} onChange={handleChange} hidden/>
+      <input type="file" enctype='multipart/form-data' ref={fileInputRef} onChange={handleChange} hidden/>
     </div>
   );
 }
