6f277c78728248360d402631b066ca9fe9be6f8c
robertohealy <healybobby@gmail.com>
2022-02-23 05:27:05 -0500

    updated button to autosubmit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <StartButton/>
-        <CardHolder></CardHolder>
       </header>
     </div>
   );
--- frontend/src/StartButton.js
+++ frontend/src/StartButton.js
@@ -1,38 +1,36 @@
 import axios, { Axios } from 'axios';
-import { useState, useRef } from 'react';
+import { useEffect, useState, useRef } from 'react';
 
 function StartButton() {
-  const [file, setFile] = useState();
-  const [name, setName] = useState();
+  const [file, setFile] = useState("");
+  const [firstpass, setFirstpass] = useState(true);
   const fileInputRef = useRef();
+
+  useEffect(() => {
+    if(!firstpass){
+      send();
+    }
+    setFirstpass(false);
+  },[file])
 
   const handleChange = (event) => {
     const file1 = event.target.files[0]
     setFile(file1);
-    setName("Name");
   }
 
   const send = event => {
     const data = new FormData();
-    data.append("name", name);
     data.append("file", file);
 
     axios.post("http://localhost:3000/upload", data)
       .then(res => console.log(res))
       .catch(err => console.log(err));
-
-    // axios.post("https://httpbin.org/anything", data)
-    //   .then(res => console.log(res))
-    //   .catch(err => console.log(err));
   }
   
   return (
     <div>
-      <button onClick={()=>fileInputRef.current.click()}>
-        Custom File Input Button
-      </button>
-      <button onClick={send}>
-        submit!
+      <button className='bg-green-500 px-6 py-2 rounded-full shadow m-2' onClick={()=>fileInputRef.current.click()}>
+        Upload File
       </button>
       <input type="file" enctype='multipart/form-data' ref={fileInputRef} onChange={handleChange} hidden/>
     </div>
